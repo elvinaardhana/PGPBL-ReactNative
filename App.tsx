2d61b31ad083718160d0e410a9befafc4d680913
elvinaardhana <elvinanurrizaardhana@mail.ugm.ac.id>
2024-10-27 21:40:33 +0700

    portofolio

--- App.tsx
+++ App.tsx
@@ -37,7 +37,7 @@ function Section({children, title}: SectionProps): React.JSX.Element {
         style={[
           styles.sectionTitle,
           {
-            color: isDarkMode ? Colors.white : Colors.black,
+            color: 'black',
           },
         ]}>
         {title}
@@ -76,18 +76,17 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="Nama">
+           Elvina Nur Riza Ardhana
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="NIM">
+            22/499632/SV/21348 
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
+          <Section title="Prodi">
+            Sistem Informasi Geografis
           </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
+          <Section title="Link Media Sosial">
+           
           </Section>
           <LearnMoreLinks />
         </View>
